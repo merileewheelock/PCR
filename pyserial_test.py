bd3d3aecebf94be3f1fd1ef6b9b3d331353473a8
Merilee Wheelock <eelirem@gmail.com>
2018-01-09 16:02:16 -0800

    serial ports

--- pyserial_test.py
+++ pyserial_test.py
@@ -1,5 +1,5 @@
 import serial
-ser = serial.Serial('/dev/test')  # open serial port
+ser = serial.Serial('/dev/ttyUSB0')  # open serial port
 print(ser.name)         # check which port was really used
 ser.write(b'hello')     # write a string
 ser.close()             # close port
