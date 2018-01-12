3b256a17e8a843b648a1c163c2a38e8eb5d76002
Merilee Wheelock <eelirem@gmail.com>
2018-01-12 13:26:30 -0800

    usd port

--- pyserial_test.py
+++ pyserial_test.py
@@ -1,5 +1,7 @@
+# To print available ports: python -m serial.tools.list_ports
+
 import serial
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+ser = serial.Serial('/dev/tty.usbmodem1421')  # open serial port
 print(ser.name)         # check which port was really used
 ser.write(b'hello')     # write a string
 ser.close()             # close port
